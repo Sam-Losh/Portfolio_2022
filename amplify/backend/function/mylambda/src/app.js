c50b739431a08fa7411b98d3176990e0a927783a
EzekielSloh <slosh42@gmail.com>
2022-05-10 21:35:56 -0500

    Backend Updates

--- amplify/backend/function/mylambda/src/app.js
+++ amplify/backend/function/mylambda/src/app.js
@@ -9,30 +9,41 @@ See the License for the specific language governing permissions and limitations 
 
 
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
-const router = express.Router();
-const cors = require("cors");
-const nodemailer = require("nodemailer");
+// const express = require('express')
+// const bodyParser = require('body-parser')
+// const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
+// const router = express.Router();
+// const cors = require("cors");
+// const nodemailer = require("nodemailer");
 
-// declare a new express app
-const app = express()
-app.use(bodyParser.json())
-app.use(awsServerlessExpressMiddleware.eventContext())
+// // declare a new express app
+// const app = express()
+// app.use(bodyParser.json())
+// app.use(awsServerlessExpressMiddleware.eventContext())
 
-// Enable CORS for all methods
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "*")
-  next()
-});
+// // Enable CORS for all methods
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*")
+//   res.header("Access-Control-Allow-Headers", "*")
+//   next()
+// });
 
 
 
-app.use(cors());
-app.use(express.json());
-app.use("/", router);
+// app.use(cors());
+// app.use(express.json());
+// app.use("/", router);
+
+// const server = awsServerlessExpress.createServer(app);
+const awsServerlessExpress = require('aws-serverless-express');
+const app = require('./app');
+
+const server = awsServerlessExpress.createServer(app);
+
+exports.handler = (event, context) => {
+  console.log(`EVENT: ${JSON.stringify(event)}`);
+  awsServerlessExpress.proxy(server, event, context);
+};
 
 const contactEmail = nodemailer.createTransport({
     service: 'gmail',
@@ -50,7 +61,7 @@ const contactEmail = nodemailer.createTransport({
     }
   });
 
-  router.post("/contact", (req, res) => {
+  app.post("/contact", (req, res) => {
     const name = req.body.name;
     const email = req.body.email;
     const message = req.body.message; 
@@ -71,9 +82,9 @@ const contactEmail = nodemailer.createTransport({
     });
   });
 
-app.listen(www.samlosh.com, function() {
-    console.log("App started")
-});
+// app.listen(server, function() {
+//     console.log("App started")
+// });
 
 // Export the app object. When executing the application local this does nothing. However,
 // to port it to AWS Lambda we will create a wrapper around that will load the app from
